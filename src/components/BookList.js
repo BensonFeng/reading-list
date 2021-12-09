eb010bd36914ac61a2b78221dbbd327eba0da6c8
BensonFeng <bensonfeng@outlook.com>
2021-12-08 21:05:52 -0500

    deleting data

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,16 +1,20 @@
+import { doc, deleteDoc } from "firebase/firestore";
+import { db } from "../firebase/config";
 export default function BookList({ books }) {
-
   const handleClick = async (id) => {
-    console.log(id)
-  }
+    await deleteDoc(doc(db, "books", id));
+    console.log(id);
+  };
 
   return (
     <div className="book-list">
       <ul>
-        {books.map(book => (
-          <li key={book.id} onClick={() => handleClick(book.id)}>{book.title}</li>
+        {books.map((book) => (
+          <li key={book.id} onClick={() => handleClick(book.id)}>
+            {book.title}
+          </li>
         ))}
       </ul>
     </div>
-  )
+  );
 }
